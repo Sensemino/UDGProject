5cb777a1f3fd2eda9df524c0dd3203a12e134400
etudiant <bastiengendron@hotmail.fr>
2019-11-08 16:00:48 +0100

    modification de generer ids FINALE

--- Generateur/Librairie/genererIDS.php
+++ Generateur/Librairie/genererIDS.php
@@ -2,13 +2,17 @@
 function genererIDS($nbligneagenerer, $nbligne, $Prefixe,$Suffixe,&$compteurPassage){//rempli un tableau de concatenations entre prefixe et suffixe
   $IDSGene = array();
 
-  $tailleSuffixeMax = strlen($nbligne+$Suffixe);
+  $tailleSuffixeMax = strlen($nbligne+$Suffixe); //On recupere le nombre de zeros que l'on souhaite
 
-  $suffixeModifie=str_pad(strval($Suffixe),$tailleSuffixeMax,0,STR_PAD_LEFT);
+  for ($j = 0; $j < $nbligneagenerer; $j++) //tant qu'on a une ligne à generer
+  {
 
-  for ($j = 0; $j < $nbligneagenerer; $j++) {
+    $val = $Suffixe+$compteurPassage; //Suffixe est incrementee
+
+    $suffixeModifie = sprintf("%'.0".$tailleSuffixeMax."d",$val); //On ajoute a suffixe tous les zeros
     
-    $IDSGene[$j] = $Prefixe.($suffixeModifie+$compteurPassage);
+    $IDSGene[$j] = $Prefixe.$suffixeModifie; //On rempli le tableau avec les valeurs concatenee
+
     $compteurPassage++;
   }
   return $IDSGene;
