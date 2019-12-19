38b0c658f214daa72a8350921403a75e75f800ee
Romain <romainguiffant@gmail.com>
2019-12-19 14:50:19 +0100

    Ajout du nouveau projet

--- Generateur/Librairie/genererNumerique.php
+++
@@ -1,9 +0,0 @@
-<?php
-function genererNumerique($nbligne, $min, $max, $nbdecimal){//remplie un tableau de nombre aléatoire entre bornes
-  $NumGene = array();
-  for ($j = 0; $j < $nbligne; $j++) {
-    $NumGene[$j] = MDGAleatoire($min, $max, $nbdecimal);
-  }
-  return $NumGene;
-}
-?>
--- Generateur/Librairie/make_seed.php
+++
@@ -1,5 +0,0 @@
-<?php
-function make_seed($var){//fabrique une seed
-  return rand(2,100)*$var;
-}
-?>
