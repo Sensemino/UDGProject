6e499ea807a51eaa8f3e43f65f625125296a84fe
Romain <romainguiffant@gmail.com>
2020-01-15 15:45:20 +0100

    ajout commentaire genererFormule

--- Générateur/Librairie/genererFormule.php
+++ Générateur/Librairie/genererFormule.php
@@ -8,7 +8,7 @@ function genererFormule($formule,$donnees,$NombreDonnee) {
   $tableauDonneePourFormule = array();
   $formuleTmp = $formule;
 
-  for($i=0;$i<count($donnees);$i++)
+  for($i=0;$i<count($donnees);$i++)             //Boucle pour récupérer les données correspond à la formule en fonction des nom de colonne trouvé dans la formule
   {
     if(preg_match("/".$donnees[$i][1]."/mD",$formule))
     {
@@ -24,18 +24,18 @@ function genererFormule($formule,$donnees,$NombreDonnee) {
     } # fin si
   } # fin pour
 
-  for($i = 1;$i<=$NombreDonnee;$i++)
+  for($i = 1;$i<=$NombreDonnee;$i++)          //Boucle pour remplacer les noms des colonnes par les valeurs et calcul de la formule
   {
     $tableauResultatTmp = array();
-    $tableauResultatTmp = array_column($tableauDonneePourFormule,$i);
+    $tableauResultatTmp = array_column($tableauDonneePourFormule,$i);     //permet de récupérer toutes les données à la case $i dans le tableauDonneePourFormule
 
-    foreach($tableauResultatTmp as $cle=>$valeur)
+    foreach($tableauResultatTmp as $cle=>$valeur)       //boucle pour remplacer les noms des colonnes par les valeurs correspondantes
     {
       $formuleTmp = preg_replace("/".$tableauDonneePourFormule[$cle][0]."/mD",$valeur,$formuleTmp);
     } #fin pour
 
-    array_push($TableauResultatFormule,matheval($formuleTmp));
-    $formuleTmp = $formule;
+    array_push($TableauResultatFormule,matheval($formuleTmp));        //on met dans le tableauResultatFormule le resultat calculé par la fonction matheval
+    $formuleTmp = $formule;                 //On réinitialise la formuleTmp pour changer les données
 
     unset($tableauResultatTmp);
   } # fin pour
